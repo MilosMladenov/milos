a2632e6fe9cbd033ec8db541d96f619bf5b0c225
Milos <69607655+MilosMladenov@users.noreply.github.com>
2023-06-05 01:15:41 +0300

    Add files via upload

---
+++ folder/main/java/com/georgi/restaurantsAdviser/Constants.java
@@ -0,0 +1,6 @@
+package com.milos.restaurantsAdviser;
+
+    public class Constants {
+        public static final String RESTAURANT_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+%s&key=AIzaSyCFLqp7dGAeehxj73Ptd5TbTVDRxeIVybU";
+    }
+
